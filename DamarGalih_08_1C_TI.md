d2614f0800e0ffc4e848b577b0124a1c6552e3e1
DmarGalee <damargalih1502@gmail.com>
2023-09-06 11:23:11 +0700

    Simpan kode ke Github

--- DamarGalih_08_1C_TI.md
+++
@@ -1,3 +0,0 @@
-# Dasar Pemrograman Dasar - Jobsheet 2
-
-Hello saya Damar Galih Kelas 1C no absen 08 dari Teknik Informatika
---
+++ MyFirstJavaGithub08.java
@@ -0,0 +1,8 @@
+public class MyFirstJavaGithub08 {
+    
+    public static void main (String [] Args ) {
+        System.out.println("Hello From Github ");
+    }
+}
+    
+
